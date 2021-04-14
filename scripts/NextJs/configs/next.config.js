9efa80a7416b8633388ac86e83875938b74c5b9d
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-04-14 10:39:21 +0200

    docs: Add docs to `next.config.js`

--- scripts/NextJs/configs/next.config.js
+++ scripts/NextJs/configs/next.config.js
@@ -1,3 +1,8 @@
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 const {
   NEXT_PUBLIC_SENTRY_DSN,
   VERCEL_GITHUB_COMMIT_SHA,
