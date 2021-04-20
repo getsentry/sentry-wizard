8f8c0a8eb291346ece8e3d40411d174ebc630adc
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-04-20 12:13:32 +0200

    fix: Add SentryWebpackPluginOptions to module config call

--- scripts/NextJs/configs/next.config.js
+++ scripts/NextJs/configs/next.config.js
@@ -6,7 +6,7 @@
 const { withSentryConfig } = require('@sentry/nextjs');
 
 const moduleExports = {
-  // your existing module.exports
+  // Your existing module.exports
 };
 
 const SentryWebpackPluginOptions = {
@@ -21,4 +21,4 @@ const SentryWebpackPluginOptions = {
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports);
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
