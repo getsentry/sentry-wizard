eb88029a0e288d02fc57a01e4663c3871bf378e5
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-05-28 11:35:37 +0200

    Add `tracesSampleRate` for performance monitoring

--- scripts/NextJs/configs/sentry.client.config.js
+++ scripts/NextJs/configs/sentry.client.config.js
@@ -8,6 +8,10 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN || '___DSN___',
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+  // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
