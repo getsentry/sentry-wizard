5b7f099d09158c8e0ded1694174da95ec02aa7a0
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-05-31 09:36:57 +0200

    Shorten `tracesSampleRate` comment

--- scripts/NextJs/configs/sentry.client.config.js
+++ scripts/NextJs/configs/sentry.client.config.js
@@ -8,8 +8,7 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN || '___DSN___',
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
+  // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
   // Note: if you want to override the automatic release value, do not set a
