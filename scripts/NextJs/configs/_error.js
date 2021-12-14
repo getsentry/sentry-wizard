c99c2efb2c7acc631f2ef3d80bdf782f780c553d
Koen Punt <koen@koenpunt.nl>
2021-12-14 10:55:34 +0100

    do not capture 404 errors as exceptions

--- scripts/NextJs/configs/_error.js
+++ scripts/NextJs/configs/_error.js
@@ -14,11 +14,10 @@ const MyError = ({ statusCode, hasGetInitialPropsRun, err }) => {
   return <NextErrorComponent statusCode={statusCode} />;
 };
 
-MyError.getInitialProps = async ({ res, err, asPath }) => {
-  const errorInitialProps = await NextErrorComponent.getInitialProps({
-    res,
-    err,
-  });
+MyError.getInitialProps = async (context) => {
+  const errorInitialProps = await NextErrorComponent.getInitialProps(context);
+  
+  const { res, err, asPath } = context;
 
   // Workaround for https://github.com/vercel/next.js/issues/8592, mark when
   // getInitialProps has run
@@ -47,13 +46,15 @@ MyError.getInitialProps = async ({ res, err, asPath }) => {
     return errorInitialProps;
   }
 
-  // If this point is reached, getInitialProps was called without any
-  // information about what the error might be. This is unexpected and may
-  // indicate a bug introduced in Next.js, so record it in Sentry
-  Sentry.captureException(
-    new Error(`_error.js getInitialProps missing data at path: ${asPath}`),
-  );
-  await Sentry.flush(2000);
+  if (res?.statusCode !== 404) {
+    // If this point is reached, getInitialProps was called without any
+    // information about what the error might be. This is unexpected and may
+    // indicate a bug introduced in Next.js, so record it in Sentry
+    Sentry.captureException(
+      new Error(`_error.js getInitialProps missing data at path: ${asPath}`),
+    );
+    await Sentry.flush(2000);
+  }
 
   return errorInitialProps;
 };
