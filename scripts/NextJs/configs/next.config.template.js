ea5d25074af263173b1d7f75608c5445844f711c
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2023-01-10 13:20:15 -0800

    updated template

--- scripts/NextJs/configs/next.config.template.js
+++ scripts/NextJs/configs/next.config.template.js
@@ -1,7 +1,7 @@
 // This file sets a custom webpack configuration to use your Next.js app
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 const { withSentryConfig } = require('@sentry/nextjs');
 
 // ORIGINAL CONFIG
