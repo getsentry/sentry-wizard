4416ea096be5a7dccd31e2a77315f715a510592d
Lukas Stracke <lukas.stracke@sentry.io>
2023-05-02 09:02:04 +0200

    ref(sveltekit): Update `sentryHandle` after 7.50.0

--- src/templates/sveltekit-templates.ts
+++ src/templates/sveltekit-templates.ts
@@ -33,8 +33,8 @@ Sentry.init({
   tracesSampleRate: 1.0,
 });
 
-// If you have custom handlers, make sure to place them after \`sentryHandle\` in the \`sequence\` function.
-export const handle = sequence(sentryHandle);
+// If you have custom handlers, make sure to place them after \`sentryHandle()\` in the \`sequence\` function.
+export const handle = sequence(sentryHandle());
 
 // If you have a custom error handler, pass it to \`handleErrorWithSentry\`
 export const handleError = handleErrorWithSentry();
