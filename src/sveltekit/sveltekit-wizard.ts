ad9655925d4bc85925bdbfda97ee7301c71d15bd
Lukas Stracke <lukas.stracke@sentry.io>
2023-05-02 16:10:04 +0200

    feat(sveltekit): Add example page creation

--- src/sveltekit/sveltekit-wizard.ts
+++ src/sveltekit/sveltekit-wizard.ts
@@ -14,7 +14,8 @@ import {
   printWelcome,
   SentryProjectData,
 } from '../utils/clack-utils';
-import { createOrMergeSvelteKitFiles } from './sdk-setup';
+import { createExamplePage } from './sdk-example';
+import { createOrMergeSvelteKitFiles, loadSvelteConfig } from './sdk-setup';
 
 import { setupCLIConfig } from './sentry-cli-setup';
 
@@ -35,7 +36,7 @@ export async function runSvelteKitWizard(
   const packageJson = await getPackageDotJson();
   await ensurePackageIsInstalled(packageJson, '@sveltejs/kit', 'Sveltekit');
 
-  const { url: sentryUrl } = await askForSelfHosted();
+  const { url: sentryUrl, selfHosted } = await askForSelfHosted();
 
   const { projects, apiKeys } = await askForWizardLogin({
     promoCode: options.promoCode,
@@ -63,8 +64,10 @@ export async function runSvelteKitWizard(
 
   const dsn = selectedProject.keys[0].dsn.public;
 
+  const svelteConfig = await loadSvelteConfig();
+
   try {
-    await createOrMergeSvelteKitFiles(dsn);
+    await createOrMergeSvelteKitFiles(dsn, svelteConfig);
   } catch (e: unknown) {
     clack.log.error('Error while setting up the SvelteKit SDK:');
     clack.log.info(
@@ -79,9 +82,36 @@ export async function runSvelteKitWizard(
     return;
   }
 
+  try {
+    await createExamplePage(svelteConfig, {
+      selfHosted,
+      url: sentryUrl,
+      orgSlug: selectedProject.organization.slug,
+      projectId: selectedProject.id,
+    });
+  } catch (e: unknown) {
+    clack.log.error('Error while creating an example page to test Sentry:');
+    clack.log.info(
+      chalk.dim(
+        typeof e === 'object' && e != null && 'toString' in e
+          ? e.toString()
+          : typeof e === 'string'
+          ? e
+          : 'Unknown error',
+      ),
+    );
+    return;
+  }
+
   //TODO: Adjust the link once SvelteKit docs are live
+  //TODO: Also adjust the link in example page template!
   clack.outro(`
 ${chalk.green('Successfully installed the Sentry SvelteKit SDK!')}
+
+${chalk.cyan(
+  'You can validate your setup by starting your dev environment (`npm run dev`) and visiting "/sentry-example-page".',
+)}
+
 Check out the SDK documentation for further configuration:
 https://github.com/getsentry/sentry-javascript/blob/develop/packages/sveltekit/README.md
   `);
