a3d3bd1ed1195617dca4c01daf5daa9333af20f8
Liam Jones <liam@stardive.co.uk>
2023-07-04 08:38:09 +0100

    Source RN's with-environment.sh script in the "Upload Debug Symbols to Sentry" build phase

--- lib/Steps/Integrations/ReactNative.ts
+++ lib/Steps/Integrations/ReactNative.ts
@@ -420,6 +420,7 @@ The snippet will create a button that, when tapped, sends a test event to Sentry
       {
         shellPath: '/bin/sh',
         shellScript: `
+source "../node_modules/react-native/scripts/xcode/with-environment.sh"
 export SENTRY_PROPERTIES=sentry.properties
 [[ $SENTRY_INCLUDE_NATIVE_SOURCES == "true" ]] && INCLUDE_SOURCES_FLAG="--include-sources" || INCLUDE_SOURCES_FLAG=""
 ../node_modules/@sentry/cli/bin/sentry-cli debug-files upload "$INCLUDE_SOURCES_FLAG" "$DWARF_DSYM_FOLDER_PATH"
