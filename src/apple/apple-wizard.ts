c7acc22e1abe4c97918e842d284e75d7238063f6
Dhiogo Ramos Brustolin <dhiogo.brustolin@sentry.io>
2023-07-06 08:59:53 +0200

    some fixes

--- src/apple/apple-wizard.ts
+++ src/apple/apple-wizard.ts
@@ -12,6 +12,7 @@ import * as templates from './templates';
 import * as xcManager from './xcode-manager';
 import * as codeTools from './code-tools';
 import * as bash from '../utils/bash';
+import { WizardOptions } from '../utils/types';
 
 const xcode = require('xcode');
 /* eslint-enable @typescript-eslint/no-unused-vars */
@@ -25,30 +26,25 @@ import {
   printWelcome,
 } from '../utils/clack-utils';
 
-interface AppleWizardOptions {
-  promoCode?: string;
-}
-
 export async function runAppleWizard(
-  options: AppleWizardOptions,
+  options: WizardOptions,
 ): Promise<void> {
   printWelcome({
     wizardName: 'Sentry Apple Wizard',
     promoCode: options.promoCode,
   });
 
-  let hasCLI = bash.hasSentryCLI();
-  if (!hasCLI) {
+
+  if (!bash.hasSentryCLI()) {
     if (!(await askToInstallSentryCLI())) {
       clack.log.warn('Without Sentry-cli, you wont be able to upload debug symbols to Sentry. You can install it later by following the instructions at https://docs.sentry.io/cli/');
     } else {
       await bash.installSentryCLI();
-      hasCLI = true;
     }
   }
 
   const projectDir = process.cwd();
-  const xcodeProjFile = findXcodeProjFile(projectDir);
+  const xcodeProjFile = findFilesWithExtension(projectDir, ".xcodeproj")[0];
 
   if (!xcodeProjFile) {
     clack.log.error('No xcode project found. Please run this command from the root of your project.');
@@ -61,7 +57,7 @@ export async function runAppleWizard(
     return;
   }
 
-  const { project, apiKey } = await getSentryProjectAndApiKey(options.promoCode);
+  const { project, apiKey } = await getSentryProjectAndApiKey(options.promoCode, options.url);
 
   xcManager.updateXcodeProject(pbxproj, project, apiKey, true, true);
 
@@ -75,8 +71,8 @@ export async function runAppleWizard(
 }
 
 //Prompt for Sentry project and API key
-async function getSentryProjectAndApiKey(promoCode: string | undefined): Promise<{ project: SentryProjectData, apiKey: { token: string } }> {
-  const { url: sentryUrl } = await askForSelfHosted();
+async function getSentryProjectAndApiKey(promoCode?: string, url?: string): Promise<{ project: SentryProjectData, apiKey: { token: string } }> {
+  const { url: sentryUrl } = await askForSelfHosted(url);
 
   const { projects, apiKeys } = await askForWizardLogin({
     promoCode: promoCode,
@@ -91,20 +87,5 @@ async function getSentryProjectAndApiKey(promoCode: string | undefined): Promise
 //find files with the given extension
 function findFilesWithExtension(dir: string, extension: string): string[] {
   const files = fs.readdirSync(dir);
-  const foundFiles: string[] = [];
-  for (const file of files) {
-    if (file.endsWith(extension)) {
-      foundFiles.push(file);
-    }
-  }
-  return foundFiles;
-}
-
-//Find a file that contains the xcodeproj extension
-function findXcodeProjFile(dir: string): string | null {
-  const files = findFilesWithExtension(dir, ".xcodeproj");
-  if (files.length > 0) {
-    return files[0];
-  }
-  return null;
+  return files.filter(file => file.endsWith(extension));
 }
