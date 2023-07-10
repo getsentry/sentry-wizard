0a894db0f532039c6416628a25431a5eb2a2c291
Liam Jones <liam@stardive.co.uk>
2023-07-10 11:32:22 +0100

    Implement ShellCheck suggestions
    `source` and `[[ ]]` are not supported in `sh`

--- lib/Steps/Integrations/ReactNative.ts
+++ lib/Steps/Integrations/ReactNative.ts
@@ -422,10 +422,10 @@ The snippet will create a button that, when tapped, sends a test event to Sentry
         shellScript: `
 WITH_ENVIRONMENT="../node_modules/react-native/scripts/xcode/with-environment.sh"
 if [ -f "$WITH_ENVIRONMENT" ]; then
-    source "$WITH_ENVIRONMENT"
+    . "$WITH_ENVIRONMENT"
 fi 
 export SENTRY_PROPERTIES=sentry.properties
-[[ $SENTRY_INCLUDE_NATIVE_SOURCES == "true" ]] && INCLUDE_SOURCES_FLAG="--include-sources" || INCLUDE_SOURCES_FLAG=""
+[ "$SENTRY_INCLUDE_NATIVE_SOURCES" = "true" ] && INCLUDE_SOURCES_FLAG="--include-sources" || INCLUDE_SOURCES_FLAG=""
 ../node_modules/@sentry/cli/bin/sentry-cli debug-files upload "$INCLUDE_SOURCES_FLAG" "$DWARF_DSYM_FOLDER_PATH"
 `,
       },
