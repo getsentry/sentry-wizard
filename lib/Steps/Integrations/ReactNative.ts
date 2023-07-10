36c26ec10eaace778886bc466720d8f3c5b1954f
Liam Jones <liam@stardive.co.uk>
2023-07-10 11:28:14 +0100

    Check `with-environment.sh` exists before sourcing
    Sentry currently supports RN 0.65+, `with-environment.sh` didn't exist until 0.69+
    
    Co-authored-by: Kryštof Woldřich <31292499+krystofwoldrich@users.noreply.github.com>

--- lib/Steps/Integrations/ReactNative.ts
+++ lib/Steps/Integrations/ReactNative.ts
@@ -420,7 +420,10 @@ The snippet will create a button that, when tapped, sends a test event to Sentry
       {
         shellPath: '/bin/sh',
         shellScript: `
-source "../node_modules/react-native/scripts/xcode/with-environment.sh"
+WITH_ENVIRONMENT="../node_modules/react-native/scripts/xcode/with-environment.sh"
+if [ -f "$WITH_ENVIRONMENT" ]; then
+    source "$WITH_ENVIRONMENT"
+fi 
 export SENTRY_PROPERTIES=sentry.properties
 [[ $SENTRY_INCLUDE_NATIVE_SOURCES == "true" ]] && INCLUDE_SOURCES_FLAG="--include-sources" || INCLUDE_SOURCES_FLAG=""
 ../node_modules/@sentry/cli/bin/sentry-cli debug-files upload "$INCLUDE_SOURCES_FLAG" "$DWARF_DSYM_FOLDER_PATH"
