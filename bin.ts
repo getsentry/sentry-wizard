faa4667427aad7e482c1043584f9bc6bc478b5b3
Lukas Stracke <lukas.stracke@sentry.io>
2023-07-11 16:06:13 +0200

    fix linter error

--- bin.ts
+++ bin.ts
@@ -5,7 +5,6 @@ import { runNextjsWizard } from './src/nextjs/nextjs-wizard';
 import { runSourcemapsWizard } from './src/sourcemaps/sourcemaps-wizard';
 import { runSvelteKitWizard } from './src/sveltekit/sveltekit-wizard';
 import { runAppleWizard } from './src/apple/apple-wizard';
-import { withTelemetry } from './src/telemetry';
 import { WizardOptions } from './src/utils/types';
 export * from './lib/Setup';
 
