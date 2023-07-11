438643e9d835fec67aa7f617659b59d415db177c
Lukas Stracke <lukas.stracke@sentry.io>
2023-07-11 11:56:53 +0200

    feat(sourcemaps): Add CLI-based flow for Angular (#349)
    Add a CLI-based source maps upload configuration flow for Angular.
    
    The reason is that for Angular devs, it might not be apparent that the "best" option for them previously was "None of the above". Some clicked "Webpack" although Angular doesn't offer access to the webpack config by default. Hence this option, similarly to CRA, simply redirects to the CLI flow and adds a custom source maps generation step.

---
+++ src/sourcemaps/tools/angular.ts
@@ -0,0 +1,42 @@
+// @ts-ignore - clack is ESM and TS complains about that. It works though
+import clack from '@clack/prompts';
+import chalk from 'chalk';
+import { abortIfCancelled } from '../../utils/clack-utils';
+
+const angularJsonTemplate = chalk.gray(`{
+  "projects": {
+    "your-project": {
+      "architect": {
+        "build": {
+          "options": {
+            ${chalk.greenBright(`"sourceMap": true`)}
+          },
+        },
+      }
+    }
+  }
+}`);
+
+export async function configureAngularSourcemapGenerationFlow(): Promise<void> {
+  clack.log.info(
+    `Enable generating source maps in your ${chalk.bold('angular.json')} file:`,
+  );
+
+  // Intentially logging directly to console here so that the code can be copied/pasted directly
+  // eslint-disable-next-line no-console
+  console.log(angularJsonTemplate);
+
+  await abortIfCancelled(
+    clack.select({
+      message: `Verify that you are generating source maps when building your Angular app.`,
+      options: [
+        {
+          label: 'I checked!',
+          hint: 'My build output folder contains .js.map files after a build.',
+          value: true,
+        },
+      ],
+      initialValue: true,
+    }),
+  );
+}
