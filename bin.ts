getsentry/sentry-wizard

commit d93538f4185f76bff2388c9f804002841455679b

ref(sourcemaps): Always collect source maps wizard telemetry (#348)
Previously, we only collected telemetry when the source maps wizard was directly started with the -i sourcemaps arg. This is probably the majority of cases anyway. However, for the sake of consistency, we should always collect telemetry. Therefore, this patch slightly refactors the location of the withTelemetry call so that we're now always using it.

bin.ts
@@ -69,6 +69,7 @@ const argv = require('yargs')
 const wizardOptions: WizardOptions = {
   url: argv.url as string | undefined,
   promoCode: argv['promo-code'] as string | undefined,
+  telemetryEnabled: !argv['disable-telemetry'],
 };
 
 switch (argv.i) {
@@ -81,25 +82,19 @@ switch (argv.i) {
     runSvelteKitWizard(wizardOptions).catch(console.error);
     break;
   case 'sourcemaps':
-    withTelemetry(
-      {
-        enabled: !argv['disable-telemetry'],
-        integration: 'sourcemaps',
-      },
-      () => runSourcemapsWizard(wizardOptions),
-      // eslint-disable-next-line no-console
-    ).catch(console.error);
+    // eslint-disable-next-line no-console
+    runSourcemapsWizard(wizardOptions).catch(console.error);
     break;
   case 'ios':
     withTelemetry(
       {
         enabled: !argv['disable-telemetry'],
-        integration: 'ios'
+        integration: 'ios',
       },
       () => runAppleWizard(wizardOptions),
       // eslint-disable-next-line no-console
     ).catch(console.error);
-    break
+    break;
   default:
     void run(argv);
 }

lib/Steps/Integrations/SourceMapsShim.ts
@@ -17,6 +17,7 @@ export class SourceMapsShim extends BaseIntegration {
     await runSourcemapsWizard({
       promoCode: this._argv.promoCode,
       url: this._argv.url,
+      telemetryEnabled: !this._argv.disableTelemetry,
     });
     return {};
   }

lib/Helper/__tests__/SentryCli.ts
@@ -14,7 +14,7 @@ const args: Args = {
   uninstall: false,
   url: 'https://localhost:1234',
   signup: false,
-  disableTelemetry: false
+  disableTelemetry: false,
 };
 
 const demoAnswers: Answers = {

lib/Steps/Integrations/Apple.ts
@@ -5,28 +5,31 @@ import { runAppleWizard } from '../../../src/apple/apple-wizard';
 import { withTelemetry } from '../../../src/telemetry';
 
 export class Apple extends BaseIntegration {
-    argv: Args;
-    public constructor(protected _argv: Args) {
-        super(_argv);
-        this.argv = _argv;
-    }
+  argv: Args;
+  public constructor(protected _argv: Args) {
+    super(_argv);
+    this.argv = _argv;
+  }
 
-    public async emit(_answers: Answers): Promise<Answers> {
-        await withTelemetry(
-            {
-                enabled: !this.argv.disableTelemetry,
-                integration: 'ios'
-            },
-            async () =>
-                await runAppleWizard({ promoCode: this._argv.promoCode, url: this._argv.url, })
-            ,
-            // eslint-disable-next-line no-console
-        ).catch(console.error);
+  public async emit(_answers: Answers): Promise<Answers> {
+    await withTelemetry(
+      {
+        enabled: !this.argv.disableTelemetry,
+        integration: 'ios',
+      },
+      async () =>
+        await runAppleWizard({
+          promoCode: this._argv.promoCode,
+          url: this._argv.url,
+          telemetryEnabled: !this._argv.disableTelemetry,
+        }),
+      // eslint-disable-next-line no-console
+    ).catch(console.error);
 
-        return {};
-    }
+    return {};
+  }
 
-    public async shouldConfigure(_answers: Answers): Promise<Answers> {
-        return this._shouldConfigure;
-    }
+  public async shouldConfigure(_answers: Answers): Promise<Answers> {
+    return this._shouldConfigure;
+  }
 }

lib/Steps/Integrations/NextJsShim.ts
@@ -17,6 +17,7 @@ export class NextJsShim extends BaseIntegration {
     await runNextjsWizard({
       promoCode: this._argv.promoCode,
       url: this._argv.url,
+      telemetryEnabled: !this._argv.disableTelemetry,
     });
     return {};
   }

lib/Steps/Integrations/SvelteKitShim.ts
@@ -17,6 +17,7 @@ export class SvelteKitShim extends BaseIntegration {
     await runSvelteKitWizard({
       promoCode: this._argv.promoCode,
       url: this._argv.url,
+      telemetryEnabled: !this._argv.disableTelemetry,
     });
     return {};
   }

src/sourcemaps/sourcemaps-wizard.ts
@@ -23,7 +23,7 @@ import { configureEsbuildPlugin } from './tools/esbuild';
 import { WizardOptions } from '../utils/types';
 import { configureCRASourcemapGenerationFlow } from './tools/create-react-app';
 import { ensureMinimumSdkVersionIsInstalled } from './utils/sdk-version';
-import { traceStep } from '../telemetry';
+import { traceStep, withTelemetry } from '../telemetry';
 import { URL } from 'url';
 import { checkIfMoreSuitableWizardExistsAndAskForRedirect } from './utils/other-wizards';
 import { configureAngularSourcemapGenerationFlow } from './tools/angular';
@@ -39,6 +39,18 @@ type SupportedTools =
   | 'angular';
 
 export async function runSourcemapsWizard(
+  options: WizardOptions,
+): Promise<void> {
+  return withTelemetry(
+    {
+      enabled: options.telemetryEnabled,
+      integration: 'sourcemaps',
+    },
+    () => runSourcemapsWizardWithTelemetry(options),
+  );
+}
+
+async function runSourcemapsWizardWithTelemetry(
   options: WizardOptions,
 ): Promise<void> {
   printWelcome({

src/utils/types.ts
@@ -1,5 +1,10 @@
 export type WizardOptions = {
   /**
+   * Controls whether the wizard should send telemetry data to Sentry.
+   */
+  telemetryEnabled: boolean;
+
+  /**
    * The promo code to use while signing up for Sentry.
    * This can be passed via the --promo-code arg.
    */

commit 64cabf9ac62bd321536ac33f6a84cf5c1d89be3f

fix: Ensure wizard exits after setup is completed (#360)
When the wizard was started with the `-i` flag, it would sometimes just keep on awaiting a promise after the `run[integration]Wizard` function returned. Seems like we broke the promise chain in #348 or sometime before that. This caused the application to appear hanging and users needing to cancel the wizard manually. 

Affected wizards.
* NextJs
* Sveltekit
* Sourcemaps
* Apple (not tested but I'm fairly certain)

This patch makes sure that the wizard exits by removing our duplicated invokation logic for the `clack`-based wizard. For the moment, we always go through the Shim classes. I'd like to refactor this eventually and get rid of the shims but for now we should fix this to unblock users.

bin.ts
@@ -1,11 +1,6 @@
 #!/usr/bin/env node
 import { Integration, Platform } from './lib/Constants';
 import { run } from './lib/Setup';
-import { runNextjsWizard } from './src/nextjs/nextjs-wizard';
-import { runSourcemapsWizard } from './src/sourcemaps/sourcemaps-wizard';
-import { runSvelteKitWizard } from './src/sveltekit/sveltekit-wizard';
-import { runAppleWizard } from './src/apple/apple-wizard';
-import { WizardOptions } from './src/utils/types';
 export * from './lib/Setup';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
@@ -63,31 +58,4 @@ const argv = require('yargs')
     describe: 'A promo code that will be applied during signup',
   }).argv;
 
-// Collect argv options that are relevant for the new wizard
-// flows based on `clack`
-const wizardOptions: WizardOptions = {
-  url: argv.url as string | undefined,
-  promoCode: argv['promo-code'] as string | undefined,
-  telemetryEnabled: !argv['disable-telemetry'],
-};
-
-switch (argv.i) {
-  case 'nextjs':
-    // eslint-disable-next-line no-console
-    runNextjsWizard(wizardOptions).catch(console.error);
-    break;
-  case 'sveltekit':
-    // eslint-disable-next-line no-console
-    runSvelteKitWizard(wizardOptions).catch(console.error);
-    break;
-  case 'sourcemaps':
-    // eslint-disable-next-line no-console
-    runSourcemapsWizard(wizardOptions).catch(console.error);
-    break;
-  case 'ios':
-    // eslint-disable-next-line no-console
-    runAppleWizard(wizardOptions).catch(console.error);
-    break;
-  default:
-    void run(argv);
-}
+void run(argv);

lib/Steps/Integrations/SourceMapsShim.ts
@@ -6,7 +6,7 @@ import { BaseIntegration } from './BaseIntegration';
 
 /**
  * This class just redirects to the `sourcemaps-wizard.ts` flow
- * for anyone calling the wizard without the '-i sveltekit' flag.
+ * for anyone calling the wizard without the '-i sourcemaps' flag.
  */
 export class SourceMapsShim extends BaseIntegration {
   public constructor(protected _argv: Args) {