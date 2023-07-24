e20b96c7de44b0251d6f6eced5e51d9188b500c3
Lukas Stracke <lukas.stracke@sentry.io>
2023-07-24 18:04:09 +0200

    chore: Ajust `lint:prettier` command to check files in `src`

--- src/utils/bash.ts
+++ src/utils/bash.ts
@@ -3,55 +3,55 @@ import * as https from 'https';
 import * as fs from 'fs';
 
 export function hasSentryCLI(): boolean {
-    try {
-        child_process.execSync('sentry-cli --version');
-        return true;
-    } catch (e) {
-        return false;
-    }
+  try {
+    child_process.execSync('sentry-cli --version');
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
 
 export async function installSentryCLI(): Promise<void> {
-    const httpAsync = new Promise((resolve, reject) => {
-        const file = fs.createWriteStream('installcli.sh');
-        https.get('https://sentry.io/get-cli/', (response) => {
-            response.pipe(file);
-            file.on('finish', () => {
-                file.close();
-                try {
-                    child_process.execSync('bash ./installcli.sh');
-                } catch (e) {
-                    reject(e);
-                    return
-                }
-                fs.unlinkSync('installcli.sh');
-                resolve(null);
-            });
+  const httpAsync = new Promise((resolve, reject) => {
+    const file = fs.createWriteStream('installcli.sh');
+    https.get('https://sentry.io/get-cli/', (response) => {
+      response.pipe(file);
+      file.on('finish', () => {
+        file.close();
+        try {
+          child_process.execSync('bash ./installcli.sh');
+        } catch (e) {
+          reject(e);
+          return;
+        }
+        fs.unlinkSync('installcli.sh');
+        resolve(null);
+      });
 
-            file.on('error', (err) => {
-                fs.unlinkSync('installcli.sh');
-                reject(err);
-            });
-        });
+      file.on('error', (err) => {
+        fs.unlinkSync('installcli.sh');
+        reject(err);
+      });
     });
+  });
 
-    await httpAsync;
+  await httpAsync;
 }
 
 export function executeSync(command: string): string {
-    const output = child_process.execSync(command);
-    return output.toString();
+  const output = child_process.execSync(command);
+  return output.toString();
 }
 
 export function execute(command: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-        child_process.exec(command, (error, stdout, _) => {
-            if (error) {
-                reject(error);
-                return;
-            }
+  return new Promise((resolve, reject) => {
+    child_process.exec(command, (error, stdout, _) => {
+      if (error) {
+        reject(error);
+        return;
+      }
 
-            resolve(stdout);
-        });
+      resolve(stdout);
     });
+  });
 }
