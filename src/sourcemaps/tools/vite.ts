6581f1c56baa8c7ab4412a6cce81fd6183171233
Lukas Stracke <lukas.stracke@sentry.io>
2023-08-04 09:42:59 +0200

    ref(sourcemaps): Handle no vite config found case

--- src/sourcemaps/tools/vite.ts
+++ src/sourcemaps/tools/vite.ts
@@ -25,10 +25,12 @@ import { findScriptFile, hasSentryContent } from '../../utils/ast-utils';
 import * as path from 'path';
 import * as fs from 'fs';
 import { debug } from '../../utils/debug';
+import { stripAnsii } from '../../utils/string';
 
-const getCodeSnippet = (options: SourceMapUploadToolConfigurationOptions) =>
-  chalk.gray(`
-import { defineConfig } from "vite";
+const getCopyPasteCodeSnippet = (
+  options: SourceMapUploadToolConfigurationOptions,
+) =>
+  chalk.gray(`import { defineConfig } from "vite";
 ${chalk.greenBright('import { sentryVitePlugin } from "@sentry/vite-plugin"')};
 
 export default defineConfig({
@@ -50,6 +52,10 @@ export default defineConfig({
 });
 `);
 
+const getNewViteConfigContent = (
+  options: SourceMapUploadToolConfigurationOptions,
+) => stripAnsii(getCopyPasteCodeSnippet(options));
+
 export const configureVitePlugin: SourceMapUploadToolConfigurationFunction =
   async (options) => {
     await installPackage({
@@ -60,15 +66,15 @@ export const configureVitePlugin: SourceMapUploadToolConfigurationFunction =
       ),
     });
 
-    const viteConfigPath = findScriptFile(
-      path.resolve(process.cwd(), 'vite.config'),
-    );
+    const viteConfigPath =
+      findScriptFile(path.resolve(process.cwd(), 'vite.config')) ||
+      (await askForViteConfigPath());
 
     let successfullyAdded = false;
     if (viteConfigPath) {
       successfullyAdded = await addVitePluginToConfig(viteConfigPath, options);
     } else {
-      Sentry.setTag('ast-mod-fail-reason', 'config-not-found');
+      successfullyAdded = await createNewViteConfig(options);
     }
 
     if (successfullyAdded) {
@@ -83,6 +89,35 @@ export const configureVitePlugin: SourceMapUploadToolConfigurationFunction =
 
     await addDotEnvSentryBuildPluginFile(options.authToken);
   };
+
+async function createNewViteConfig(
+  options: SourceMapUploadToolConfigurationOptions,
+): Promise<boolean> {
+  try {
+    await fs.promises.writeFile(
+      'vite.config.js',
+      getNewViteConfigContent(options),
+    );
+    Sentry.setTag('created-new-config', 'success');
+    return true;
+  } catch (e) {
+    debug(e);
+    Sentry.setTag('created-new-config', 'fail');
+    clack.log.warn(
+      `Could not create a new ${chalk.cyan(
+        'vite.config.js',
+      )} file. Please create one manually and follow the instructions below.`,
+    );
+
+    clack.log.info(
+      chalk.gray(
+        'More information about vite configs: https://vitejs.dev/config/',
+      ),
+    );
+
+    return false;
+  }
+}
 
 async function addVitePluginToConfig(
   viteConfigPath: string,
@@ -161,13 +196,46 @@ async function showCopyPasteInstructions(
 
   // Intentionally logging directly to console here so that the code can be copied/pasted directly
   // eslint-disable-next-line no-console
-  console.log(getCodeSnippet(options));
+  console.log('\n', getCopyPasteCodeSnippet(options));
 
   await abortIfCancelled(
     select({
       message: 'Did you copy the snippet above?',
       options: [{ label: 'Yes, continue!', value: true }],
       initialValue: true,
+    }),
+  );
+}
+
+async function askForViteConfigPath(): Promise<string | undefined> {
+  const hasViteConfig = await abortIfCancelled(
+    clack.confirm({
+      message: `Do you have a vite config file (e.g. ${chalk.cyan(
+        'vite.config.js',
+      )}?`,
+      initialValue: true,
+    }),
+  );
+
+  if (!hasViteConfig) {
+    return undefined;
+  }
+
+  return await abortIfCancelled(
+    clack.text({
+      message: 'Please enter the path to your vite config file:',
+      placeholder: `.${path.sep}vite.config.js`,
+      validate: (value) => {
+        if (!value) {
+          return 'Please enter a path.';
+        }
+
+        try {
+          fs.accessSync(value);
+        } catch {
+          return 'Could not access the file at this path.';
+        }
+      },
     }),
   );
 }
