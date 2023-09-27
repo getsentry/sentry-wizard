3da770a26a60ff7948ab7bf8ba581a31fb54f9b3
Onur Temizkan <onur@narval.co.uk>
2023-09-27 12:03:25 +0100

    fix(remix): Fix `request` arg in `handleError` template.

--- src/remix/templates.ts
+++ src/remix/templates.ts
@@ -6,6 +6,6 @@ export const ERROR_BOUNDARY_TEMPLATE_V2 = `const ErrorBoundary = () => {
 `;
 
 export const HANDLE_ERROR_TEMPLATE_V2 = `function handleError(error, { request }) {
-  Sentry.captureRemixServerException(error, 'remix.server', { request });
+  Sentry.captureRemixServerException(error, 'remix.server', request);
 }
 `;
