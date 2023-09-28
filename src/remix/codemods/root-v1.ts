40a68d44954c0a7623f379b240123d928f96490a
Onur Temizkan <onur@narval.co.uk>
2023-09-28 16:27:30 +0100

    fix(remix): Instrument existing root ErrorBoundary

--- src/remix/codemods/root-v1.ts
+++ src/remix/codemods/root-v1.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
-import * as recast from 'recast';
 import * as path from 'path';
 
 // @ts-expect-error - clack is ESM and TS complains about that. It works though
@@ -8,7 +7,8 @@ import clack from '@clack/prompts';
 import chalk from 'chalk';
 
 // @ts-expect-error - magicast is ESM and TS complains about that. It works though
-import { builders, generateCode, loadFile, writeFile } from 'magicast';
+import { loadFile, writeFile } from 'magicast';
+import { wrapAppWithSentry } from './root-common';
 
 export async function instrumentRootRouteV1(
   rootFileName: string,
@@ -18,57 +18,7 @@ export async function instrumentRootRouteV1(
       path.join(process.cwd(), 'app', rootFileName),
     );
 
-    rootRouteAst.imports.$add({
-      from: '@sentry/remix',
-      imported: 'withSentry',
-      local: 'withSentry',
-    });
-
-    recast.visit(rootRouteAst.$ast, {
-      visitExportDefaultDeclaration(path) {
-        /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-        if (path.value.declaration.type === 'FunctionDeclaration') {
-          // Move the function declaration just before the default export
-          path.insertBefore(path.value.declaration);
-
-          // Get the name of the function to be wrapped
-          const functionName: string = path.value.declaration.id.name as string;
-
-          // Create the wrapped function call
-          const functionCall = recast.types.builders.callExpression(
-            recast.types.builders.identifier('withSentry'),
-            [recast.types.builders.identifier(functionName)],
-          );
-
-          // Replace the default export with the wrapped function call
-          path.value.declaration = functionCall;
-        } else if (path.value.declaration.type === 'Identifier') {
-          const rootRouteExport = rootRouteAst.exports.default;
-
-          const expressionToWrap = generateCode(
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-            rootRouteExport.$ast,
-          ).code;
-
-          rootRouteAst.exports.default = builders.raw(
-            `withSentry(${expressionToWrap})`,
-          );
-        } else {
-          clack.log.warn(
-            chalk.yellow(
-              `Couldn't instrument ${chalk.bold(
-                rootFileName,
-              )} automatically. Wrap your default export with: ${chalk.dim(
-                'withSentry()',
-              )}\n`,
-            ),
-          );
-        }
-
-        this.traverse(path);
-        /* eslint-enable @typescript-eslint/no-unsafe-member-access */
-      },
-    });
+    wrapAppWithSentry(rootRouteAst, rootFileName);
 
     await writeFile(
       rootRouteAst.$ast,
