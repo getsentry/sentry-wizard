b6a2940d1a58fb25843810cf067b38900cf4f28d
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2024-03-07 16:16:40 +0530

    fixed empty lines

--- src/nextjs/templates.ts
+++ src/nextjs/templates.ts
@@ -160,9 +160,7 @@ Sentry.init({
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-  ${additionalOptions}
-  ${spotlightOption}
+  debug: false,${additionalOptions}${spotlightOption}
 });
 `;
 }
