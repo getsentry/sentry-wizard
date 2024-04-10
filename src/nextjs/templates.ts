43cf3ab62986b260c043e804b4efa80dc3d86f50
Lukas Stracke <lukas.stracke@sentry.io>
2024-04-10 10:33:56 +0200

    feat(nextjs): Ask users about `tunnelRoute` option

--- src/nextjs/templates.ts
+++ src/nextjs/templates.ts
@@ -17,7 +17,13 @@ export function getNextjsWebpackPluginOptionsTemplate(
   }`;
 }
 
-export function getNextjsSentryBuildOptionsTemplate(): string {
+type SentryNextjsBuildOptions = {
+  tunnelRoute: boolean;
+};
+
+export function getNextjsSentryBuildOptionsTemplate({
+  tunnelRoute,
+}: SentryNextjsBuildOptions): string {
   return `{
     // For all available options, see:
     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
@@ -28,10 +34,13 @@ export function getNextjsSentryBuildOptionsTemplate(): string {
     // Transpiles SDK to be compatible with IE11 (increases bundle size)
     transpileClientSDK: true,
 
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers. (increases server load)
+    // ${
+      tunnelRoute ? 'Route' : 'Uncomment to route'
+    } browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your Vercel bill.
     // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
     // side errors will fail.
-    tunnelRoute: "/monitoring",
+    ${tunnelRoute ? '' : '// '}tunnelRoute: "/monitoring",
 
     // Hides source maps from generated client bundles
     hideSourceMaps: true,
