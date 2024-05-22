b4b182b30d4af4c5b1e28ad1b4fe6375e0ce01b2
michael-harvey <harvey.miche@gmail.com>
2024-05-23 00:42:06 +1200

    Remove transpileClientSDK template property

--- src/nextjs/templates.ts
+++ src/nextjs/templates.ts
@@ -32,9 +32,6 @@ export function getWithSentryConfigOptionsTemplate({
     // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
 
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
     // ${
       tunnelRoute ? 'Route' : 'Uncomment to route'
     } browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
