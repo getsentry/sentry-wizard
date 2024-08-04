9ad96f06f6c6512bb1ce498f2b92cc3f23b4dae1
Harald Nordgren <haraldnordgren@gmail.com>
2024-08-04 20:47:04 +0200

    More lint issues

--- src/apple/xcode-manager.ts
+++ src/apple/xcode-manager.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable max-lines */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
