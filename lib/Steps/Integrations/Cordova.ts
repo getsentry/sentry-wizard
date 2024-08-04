cd14d8715c6c94267ac96bc63fa7c0799ae3a667
Harald Nordgren <haraldnordgren@gmail.com>
2024-08-04 17:22:21 +0200

    More lint issues

--- lib/Steps/Integrations/Cordova.ts
+++ lib/Steps/Integrations/Cordova.ts
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import * as fs from 'fs';
 import type { Answers } from 'inquirer';
 import * as path from 'path';
