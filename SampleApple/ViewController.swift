cc66e5abd2d04c007f70d4dc73faa0060ebee7c0
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-01-31 15:44:30 +0100

    Revert "Revert "added sample app""
    This reverts commit 74189d5ae672c0afab7a475a49c6b3011abb010f.

---
+++ SampleApple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleApple
+//
+//  Created by Philip Niedertscheider on 31.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
