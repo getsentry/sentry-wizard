7385f72a5d74165fc70c65211078ea5dc7af04db
Onur Temizkan <onur@narval.co.uk>
2025-02-04 11:02:18 +0000

    feat(angular): Add Sentry setup in App Config (#769)
    * feat(angular): Add Sentry setup in App Config
    
    * Use `hasSentryContent` from `ast-utils`
    
    * Bail out if `app-config` is not under pre-defined location

--- src/angular/sdk-setup.ts
+++ src/angular/sdk-setup.ts
@@ -3,14 +3,19 @@
 // @ts-expect-error - magicast is ESM and TS complains about that. It works though
 import { loadFile, writeFile } from 'magicast';
 
+import * as fs from 'fs';
 import * as path from 'path';
 
 // @ts-expect-error - clack is ESM and TS complains about that. It works though
 import clack from '@clack/prompts';
 import chalk from 'chalk';
+import { updateAppConfigMod } from './codemods/app-config';
 import { updateAppEntryMod } from './codemods/main';
 import { hasSentryContent } from '../utils/ast-utils';
+import * as Sentry from '@sentry/node';
+
 import type { namedTypes as t } from 'ast-types';
+import type { SemVer } from 'semver';
 
 export async function initalizeSentryOnApplicationEntry(
   dsn: string,
@@ -30,20 +35,86 @@ export async function initalizeSentryOnApplicationEntry(
 Skipping adding Sentry functionality to ${chalk.cyan(appEntryFilename)}.`,
     );
 
-    return;
-  }
-
-  try {
     const updatedAppEntryMod = updateAppEntryMod(
       originalAppEntry,
       dsn,
       selectedFeatures,
     );
 
-    await writeFile(updatedAppEntryMod.$ast, appEntryPath);
+    try {
+      await writeFile(updatedAppEntryMod.$ast, appEntryPath);
+    } catch (error: unknown) {
+      clack.log.error(
+        `Error while adding Sentry to ${chalk.cyan(appEntryFilename)}`,
+      );
+
+      clack.log.warn(
+        `Please refer to the documentation for manual setup:
+${chalk.underline(
+  'https://docs.sentry.io/platforms/javascript/guides/angular/#configure',
+)}`,
+      );
+    }
+
+    clack.log.success(
+      `Successfully initialized Sentry on ${chalk.cyan(appEntryFilename)}`,
+    );
+  }
+}
+
+export async function updateAppConfig(
+  angularVersion: SemVer,
+  isTracingEnabled: boolean,
+): Promise<void> {
+  const appConfigFilename = 'app.config.ts';
+  const appConfigPath = path.join(
+    process.cwd(),
+    'src',
+    'app',
+    appConfigFilename,
+  );
+
+  if (!fs.existsSync(appConfigPath)) {
+    Sentry.setTag('angular-app-config-found', false);
+
+    clack.log.warn(
+      `File ${chalk.cyan(
+        appConfigFilename,
+      )} not found. Skipping adding Sentry functionality.`,
+    );
+
+    clack.log.warn(`Please refer to the documentation for manual setup:
+${chalk.underline(
+  'https://docs.sentry.io/platforms/javascript/guides/angular/#configure',
+)}`);
+
+    return;
+  }
+
+  Sentry.setTag('angular-app-config-found', true);
+
+  const appConfig = await loadFile(appConfigPath);
+
+  if (hasSentryContent(appConfig.$ast as t.Program)) {
+    clack.log.warn(
+      `File ${chalk.cyan(appConfigFilename)} already contains Sentry.
+  Skipping adding Sentry functionality to ${chalk.cyan(appConfigFilename)}.`,
+    );
+
+    return;
+  }
+
+  try {
+    const updatedAppConfigMod = updateAppConfigMod(
+      appConfig,
+      angularVersion,
+      isTracingEnabled,
+    );
+
+    await writeFile(updatedAppConfigMod.$ast, appConfigPath);
   } catch (error: unknown) {
     clack.log.error(
-      `Error while adding Sentry to ${chalk.cyan(appEntryFilename)}`,
+      `Error while updating your app config ${chalk.cyan(appConfigFilename)}.`,
     );
 
     clack.log.info(
@@ -56,17 +127,15 @@ Skipping adding Sentry functionality to ${chalk.cyan(appEntryFilename)}.`,
       ),
     );
 
-    clack.log.warn(
-      `Please refer to the documentation for manual setup:
+    clack.log.warn(`Please refer to the documentation for manual setup:
 ${chalk.underline(
   'https://docs.sentry.io/platforms/javascript/guides/angular/#configure',
-)}`,
-    );
+)}`);
 
     return;
   }
 
   clack.log.success(
-    `Successfully initialized Sentry on ${chalk.cyan(appEntryFilename)}`,
+    `Successfully updated your app config ${chalk.cyan(appConfigFilename)}`,
   );
 }
