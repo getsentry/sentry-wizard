0fdacc9a3e8e4edb00c23562a0902afc3829bc70
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-02-07 09:58:50 +0100

    revert more e2e test assets

--- e2e-tests/test-applications/nuxt-3-test-app/server/api/sentry-example-api.ts
+++
@@ -1,7 +0,0 @@
-// This is just a very simple API route that throws an example error.
-// Feel free to delete this file.
-import { defineEventHandler } from '#imports';
-
-export default defineEventHandler(() => {
-  throw new Error("Sentry Example API Route Error");
-});
