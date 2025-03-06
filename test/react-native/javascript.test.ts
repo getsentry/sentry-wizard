77adf02d7852942c6364c3d3f6cb6d3479b2f4ce
Antonis Lilis <antonis.lilis@gmail.com>
2025-03-06 19:35:24 +0200

    Add exact test for the default export of a new Expo 52 project

--- test/react-native/javascript.test.ts
+++ test/react-native/javascript.test.ts
@@ -323,7 +323,27 @@ export default Sentry.wrap(Another.wrap(() => {
 
     it('wraps a complex root function', () => {
       // This is the default export for a new Expo 52 project
-      const mod = parseModule(`import * as Sentry from '@sentry/react-native';
+      const mod =
+        parseModule(`import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://sentry.io/123',
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -353,7 +373,26 @@ export default function RootLayout() {
 }
 `);
 
-      const expectedOutput = `import * as Sentry from '@sentry/react-native';
+      const expectedOutput = `import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://sentry.io/123',
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default Sentry.wrap(function RootLayout() {
   const colorScheme = useColorScheme();
