2015388f14a2f1f66880ea9d9828b0b902ebc009
Antonis Lilis <antonis.lilis@gmail.com>
2025-03-12 17:08:36 +0200

    Simplify not defined check
    Co-authored-by: Krystof Woldrich <31292499+krystofwoldrich@users.noreply.github.com>

--- src/react-native/javascript.ts
+++ src/react-native/javascript.ts
@@ -184,7 +184,7 @@ export function checkAndWrapRootComponent(
   }
 
   const defaultExport = getDefaultExport(mod.$ast as t.Program);
-  if (defaultExport === undefined) {
+  if (!defaultExport) {
     return SentryWrapResult.NotFound;
   }
 
