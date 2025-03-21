e2465658a39e88fdce6b9920d447fdf0b13806fc
Lukas Stracke <lukas.stracke@sentry.io>
2025-03-21 12:08:00 +0100

    test(flutter): Fix failing snapshot tests (#891)

--- test/flutter/templates.test.ts
+++ test/flutter/templates.test.ts
@@ -9,13 +9,13 @@ describe('Flutter code templates', () => {
     it('generates pubspec with project and org', () => {
       const template = pubspecOptions('fixture-project', 'fixture-org');
       expect(template).toMatchInlineSnapshot(`
-      "sentry:
-        upload_debug_symbols: true
-        upload_source_maps: true
-        project: fixture-project
-        org: fixture-org
-      "
-      `);
+              "sentry:
+                upload_debug_symbols: true
+                upload_source_maps: true
+                project: fixture-project
+                org: fixture-org
+              "
+            `);
     });
   });
   describe('sentry.properties', () => {
@@ -35,20 +35,20 @@ describe('Flutter code templates', () => {
         'const MyApp()',
       );
       expect(template).toMatchInlineSnapshot(`
-      "await SentryFlutter.init(
-          (options) {
-            options.dsn = 'my-dsn';
-            // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
-            // We recommend adjusting this value in production.
-            options.tracesSampleRate = 1.0;
-            // The sampling rate for profiling is relative to tracesSampleRate
-            // Setting to 1.0 will profile 100% of sampled transactions:
-            options.profilesSampleRate = 1.0;
-          },
-          appRunner: () => runApp(SentryWidget(child: const MyApp())),
-        );
-        // TODO: Remove this line after sending the first sample event to sentry.
-        await Sentry.captureException(Exception('This is a sample exception.'));"
+        "await SentryFlutter.init(
+            (options) {
+              options.dsn = 'my-dsn';
+              // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+              // We recommend adjusting this value in production.
+              options.tracesSampleRate = 1.0;
+              // The sampling rate for profiling is relative to tracesSampleRate
+              // Setting to 1.0 will profile 100% of sampled transactions:
+              options.profilesSampleRate = 1.0;
+            },
+            appRunner: () => runApp(SentryWidget(child: const MyApp())),
+          );
+          // TODO: Remove this line after sending the first sample event to sentry.
+          await Sentry.captureException(StateError('This is a sample exception.'));"
       `);
     });
 
@@ -72,7 +72,7 @@ describe('Flutter code templates', () => {
             appRunner: () => runApp(SentryWidget(child: const MyApp())),
           );
           // TODO: Remove this line after sending the first sample event to sentry.
-          await Sentry.captureException(Exception('This is a sample exception.'));"
+          await Sentry.captureException(StateError('This is a sample exception.'));"
       `);
     });
 
@@ -93,7 +93,7 @@ describe('Flutter code templates', () => {
             appRunner: () => runApp(SentryWidget(child: const MyApp())),
           );
           // TODO: Remove this line after sending the first sample event to sentry.
-          await Sentry.captureException(Exception('This is a sample exception.'));"
+          await Sentry.captureException(StateError('This is a sample exception.'));"
       `);
     });
   });
