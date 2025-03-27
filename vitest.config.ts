d7522d4217f9e8489303c7cc1940890a682cdfd0
Lukas Stracke <lukas.stracke@sentry.io>
2025-03-27 19:28:54 +0100

    add old tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    include: ['lib/**/__tests__/**/*.ts', 'test/**/*.test.ts'],
     exclude: ['./e2e-tests/**', 'node_modules/**', 'dist/**'],
     environment: 'node',
   },
