00c246a33973f34c9ad4682bd31d48d6bc89af75
Luca Forstner <luca.forstner@sentry.io>
2025-03-27 13:03:09 +0100

    .

--- test/nextjs/templates.test.ts
+++ test/nextjs/templates.test.ts
@@ -14,36 +14,36 @@ describe('Next.js code templates', () => {
       });
 
       expect(template).toMatchInlineSnapshot(`
-          "// This file configures the initialization of Sentry on the client.
-          // The config you add here will be used whenever a users loads a page in their browser.
-          // https://docs.sentry.io/platforms/javascript/guides/nextjs/
+        "// This file configures the initialization of Sentry on the client.
+        // The added here will be used whenever a users loads a page in their browser.
+        // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-          import * as Sentry from "@sentry/nextjs";
+        import * as Sentry from "@sentry/nextjs";
 
-          Sentry.init({
-            dsn: "my-dsn",
+        Sentry.init({
+          dsn: "my-dsn",
 
-            // Add optional integrations for additional features
-            integrations: [
-              Sentry.replayIntegration(),
-            ],
+          // Add optional integrations for additional features
+          integrations: [
+            Sentry.replayIntegration(),
+          ],
 
-            // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-            tracesSampleRate: 1,
+          // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+          tracesSampleRate: 1,
 
-            // Define how likely Replay events are sampled.
-            // This sets the sample rate to be 10%. You may want this to be 100% while
-            // in development and sample at a lower rate in production
-            replaysSessionSampleRate: 0.1,
+          // Define how likely Replay events are sampled.
+          // This sets the sample rate to be 10%. You may want this to be 100% while
+          // in development and sample at a lower rate in production
+          replaysSessionSampleRate: 0.1,
 
-            // Define how likely Replay events are sampled when an error occurs.
-            replaysOnErrorSampleRate: 1.0,
+          // Define how likely Replay events are sampled when an error occurs.
+          replaysOnErrorSampleRate: 1.0,
 
-            // Setting this option to true will print useful information to the console while you're setting up Sentry.
-            debug: false,
-          });
-          "
-        `);
+          // Setting this option to true will print useful information to the console while you're setting up Sentry.
+          debug: false,
+        });
+        "
+      `);
     });
 
     it('generates client-side Sentry config with performance monitoring disabled', () => {
@@ -53,33 +53,33 @@ describe('Next.js code templates', () => {
       });
 
       expect(template).toMatchInlineSnapshot(`
-          "// This file configures the initialization of Sentry on the client.
-          // The config you add here will be used whenever a users loads a page in their browser.
-          // https://docs.sentry.io/platforms/javascript/guides/nextjs/
+        "// This file configures the initialization of Sentry on the client.
+        // The added here will be used whenever a users loads a page in their browser.
+        // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-          import * as Sentry from "@sentry/nextjs";
+        import * as Sentry from "@sentry/nextjs";
 
-          Sentry.init({
-            dsn: "my-dsn",
+        Sentry.init({
+          dsn: "my-dsn",
 
-            // Add optional integrations for additional features
-            integrations: [
-              Sentry.replayIntegration(),
-            ],
+          // Add optional integrations for additional features
+          integrations: [
+            Sentry.replayIntegration(),
+          ],
 
-            // Define how likely Replay events are sampled.
-            // This sets the sample rate to be 10%. You may want this to be 100% while
-            // in development and sample at a lower rate in production
-            replaysSessionSampleRate: 0.1,
+          // Define how likely Replay events are sampled.
+          // This sets the sample rate to be 10%. You may want this to be 100% while
+          // in development and sample at a lower rate in production
+          replaysSessionSampleRate: 0.1,
 
-            // Define how likely Replay events are sampled when an error occurs.
-            replaysOnErrorSampleRate: 1.0,
+          // Define how likely Replay events are sampled when an error occurs.
+          replaysOnErrorSampleRate: 1.0,
 
-            // Setting this option to true will print useful information to the console while you're setting up Sentry.
-            debug: false,
-          });
-          "
-        `);
+          // Setting this option to true will print useful information to the console while you're setting up Sentry.
+          debug: false,
+        });
+        "
+      `);
     });
 
     it('generates client-side Sentry config with session replay disabled', () => {
@@ -89,23 +89,23 @@ describe('Next.js code templates', () => {
       });
 
       expect(template).toMatchInlineSnapshot(`
-          "// This file configures the initialization of Sentry on the client.
-          // The config you add here will be used whenever a users loads a page in their browser.
-          // https://docs.sentry.io/platforms/javascript/guides/nextjs/
+        "// This file configures the initialization of Sentry on the client.
+        // The added here will be used whenever a users loads a page in their browser.
+        // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-          import * as Sentry from "@sentry/nextjs";
+        import * as Sentry from "@sentry/nextjs";
 
-          Sentry.init({
-            dsn: "my-dsn",
+        Sentry.init({
+          dsn: "my-dsn",
 
-            // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-            tracesSampleRate: 1,
+          // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+          tracesSampleRate: 1,
 
-            // Setting this option to true will print useful information to the console while you're setting up Sentry.
-            debug: false,
-          });
-          "
-        `);
+          // Setting this option to true will print useful information to the console while you're setting up Sentry.
+          debug: false,
+        });
+        "
+      `);
     });
   });
 
