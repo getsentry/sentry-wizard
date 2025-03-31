b93af32aea008d46dfc0fc376ff6af901bfa96da
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-03-31 11:18:37 +0200

    refactor(apple): move configure-sentry-cli to separate file (#908)

--- src/apple/apple-wizard.ts
+++ src/apple/apple-wizard.ts
@@ -6,9 +6,7 @@
 // @ts-expect-error - clack is ESM and TS complains about that. It works though
 import clack from '@clack/prompts';
 import * as Sentry from '@sentry/node';
-import chalk from 'chalk';
 import { traceStep, withTelemetry } from '../telemetry';
-import * as SentryUtils from '../utils/sentrycli-utils';
 import * as codeTools from './code-tools';
 
 /* eslint-enable @typescript-eslint/no-unused-vars */
@@ -21,6 +19,7 @@ import {
 import { checkInstalledCLI } from './check-installed-cli';
 import { configureFastlane } from './configure-fastlane';
 import { configurePackageManager } from './configure-package-manager';
+import { configureSentryCLI } from './configure-sentry-cli';
 import { lookupXcodeProject } from './lookup-xcode-project';
 import { AppleWizardOptions } from './options';
 
@@ -70,17 +69,12 @@ async function runAppleWizardWithTelementry(
     options,
     'apple-ios',
   );
-  SentryUtils.createSentryCLIRC(projectDir, { auth_token: authToken });
-  clack.log.info(
-    `Created a ${chalk.cyan(
-      '.sentryclirc',
-    )} file in your project directory to provide an auth token for Sentry CLI.
-    
-It was also added to your ${chalk.cyan('.gitignore')} file.
-Set the ${chalk.cyan(
-      'SENTRY_AUTH_TOKEN',
-    )} environment variable in your CI environment. See https://docs.sentry.io/cli/configuration/#auth-token for more information.`,
-  );
+
+  // Step - Sentry CLI Configuration Setup
+  configureSentryCLI({
+    projectDir,
+    authToken: authToken,
+  });
 
   // Step - Set up Package Manager
   const { shouldUseSPM } = await configurePackageManager({
