9f36138f7dce981cec06008e49832fb65d4d4d73
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-04-16 15:10:12 +0200

    fix test skipping

--- test/apple/macos-system-helper.test.ts
+++ test/apple/macos-system-helper.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { MacOSSystemHelpers } from '../../src/apple/macos-system-helper';
 
 import path from 'node:path';
@@ -13,81 +13,76 @@ const projectWithSynchronizedFolders = path.join(
 );
 
 // The path to the Xcode.app can be different on different machines, so we allow overwriting it using environment variables
-const xcodeAppPath = process.env.XCODE_APP_PATH ?? '/Applications/Xcode.app';
+const xcodeAppPath =
+  process.env.XCODE_APP_PATH ?? '/Applications/Xcode-16.3.0.app';
 
 describe('MacOSSystemHelpers', () => {
   describe('findSDKRootDirectoryPath', () => {
-    it('should return the SDK root directory path', () => {
-      // Skip this test if the OS is not macOS because it requires Xcode to be installed
-      if (process.platform !== 'darwin') {
-        it.skip('skipped on non-macOS platforms');
-        return;
-      }
+    test.runIf(process.platform === 'darwin')(
+      'should return the SDK root directory path',
+      () => {
+        // -- Act --
+        const sdkRootDirectoryPath =
+          MacOSSystemHelpers.findSDKRootDirectoryPath();
 
-      // -- Act --
-      const sdkRootDirectoryPath =
-        MacOSSystemHelpers.findSDKRootDirectoryPath();
-
-      // -- Assert --
-      expect(sdkRootDirectoryPath).toBe(
-        path.join(
-          xcodeAppPath,
-          'Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk',
-        ),
-      );
-    });
+        // -- Assert --
+        expect(sdkRootDirectoryPath).toBe(
+          path.join(
+            xcodeAppPath,
+            'Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk',
+          ),
+        );
+      },
+    );
   });
 
   describe('findDeveloperDirectoryPath', () => {
-    it('should return the developer directory path', () => {
-      // Skip this test if the OS is not macOS because it requires Xcode to be installed
-      if (process.platform !== 'darwin') {
-        it.skip('skipped on non-macOS platforms');
-        return;
-      }
+    test.runIf(process.platform === 'darwin')(
+      'should return the developer directory path',
+      () => {
+        // -- Act --
+        const developerDirectoryPath =
+          MacOSSystemHelpers.findDeveloperDirectoryPath();
 
-      // -- Act --
-      const developerDirectoryPath =
-        MacOSSystemHelpers.findDeveloperDirectoryPath();
-
-      // -- Assert --
-      expect(developerDirectoryPath).toBe(
-        path.join(xcodeAppPath, 'Contents/Developer'),
-      );
-    });
+        // -- Assert --
+        expect(developerDirectoryPath).toBe(
+          path.join(xcodeAppPath, 'Contents/Developer'),
+        );
+      },
+    );
   });
 
   describe('readXcodeBuildSettings', () => {
-    it('should return the build settings', () => {
-      // Skip this test if the OS is not macOS because it requires Xcode to be installed
-      if (process.platform !== 'darwin') {
-        it.skip('skipped on non-macOS platforms');
-        return;
-      }
+    test.runIf(process.platform === 'darwin')(
+      'should return the build settings',
+      () => {
+        // -- Act --
+        const buildSettings = MacOSSystemHelpers.readXcodeBuildSettings(
+          projectWithSynchronizedFolders,
+        );
 
-      // -- Act --
-      const buildSettings = MacOSSystemHelpers.readXcodeBuildSettings(
-        projectWithSynchronizedFolders,
-      );
-
-      // -- Assert --
-      // The build settings are a massive list of key-value pairs, so we'll just check a few of them
-      // which are relevant for our use cases.
-      expect(buildSettings?.['CONFIGURATION_BUILD_DIR']).toEqual(
-        path.join(
-          appleProjectsPath,
-          'project-with-synchronized-folders/build/Release-unknown',
-        ),
-      );
-      expect(buildSettings?.['TARGET_BUILD_DIR']).toEqual(
-        path.join(
-          appleProjectsPath,
-          'project-with-synchronized-folders/build/Release-unknown',
-        ),
-      );
-      expect(buildSettings?.['BUILD_DIR']).toEqual(
-        path.join(appleProjectsPath, 'project-with-synchronized-folders/build'),
-      );
-    });
+        // -- Assert --
+        // The build settings are a massive list of key-value pairs, so we'll just check a few of them
+        // which are relevant for our use cases.
+        expect(buildSettings?.['CONFIGURATION_BUILD_DIR']).toEqual(
+          path.join(
+            appleProjectsPath,
+            'project-with-synchronized-folders/build/Release-unknown',
+          ),
+        );
+        expect(buildSettings?.['TARGET_BUILD_DIR']).toEqual(
+          path.join(
+            appleProjectsPath,
+            'project-with-synchronized-folders/build/Release-unknown',
+          ),
+        );
+        expect(buildSettings?.['BUILD_DIR']).toEqual(
+          path.join(
+            appleProjectsPath,
+            'project-with-synchronized-folders/build',
+          ),
+        );
+      },
+    );
   });
 });
