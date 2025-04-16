447b488a80d62b19403d4672dee5505f7a306be6
Simon Hellmayr <simon.hellmayr@sentry.io>
2025-04-16 09:31:26 +0200

    move error declaration out of handler

--- src/nextjs/templates.ts
+++ src/nextjs/templates.ts
@@ -452,16 +452,14 @@ export function getSentryExampleAppDirApiRoute() {
   return `import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
-
+class SentryExampleAPIError extends Error {
+  constructor(message: string | undefined) {
+    super(message);
+    this.name = "SentryExampleAPIError";
+  }
+}
 // A faulty API route to test Sentry's error monitoring
 export function GET() {
-  class SentryExampleAPIError extends Error {
-    constructor(message: string | undefined) {
-      super(message);
-      this.name = "SentryExampleAPIError";
-    }
-  }
-
   throw new SentryExampleAPIError("This error is raised on the backend called by the example page.");
   return NextResponse.json({ data: "Testing Sentry Error..." });
 }
