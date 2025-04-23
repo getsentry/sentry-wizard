50bfe52a36f22d89709545551ffbe7394df55c79
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-04-23 10:50:36 +0200

    remove local dev setup file

--- vitest.workspace.ts
+++
@@ -1,8 +0,0 @@
-import { defineWorkspace } from 'vitest/config';
-
-export default defineWorkspace([
-  './vitest.config.ts',
-  './e2e-tests/vitest.config.ts',
-  './e2e-tests/test-applications/sveltekit-test-app/vite.config.ts',
-  './e2e-tests/test-applications/remix-test-app/vite.config.ts',
-]);
