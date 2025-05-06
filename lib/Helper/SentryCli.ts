6f81c62fb6d63c93447c1850530aa7386c4ac116
Philip Niedertscheider <phil.niedertscheider@sentry.io>
2025-05-06 13:17:46 +0200

    fix(apple): Use project-relative path to gitignore file (#982)

--- lib/Helper/SentryCli.ts
+++ lib/Helper/SentryCli.ts
@@ -1,15 +1,8 @@
-import * as fs from 'node:fs';
 import type { Answers } from 'inquirer';
 import * as path from 'node:path';
 
 import type { Args } from '../Constants';
-import { addToGitignore } from './Git';
-import { green, l, nl, red } from './Logging';
 import { Config } from '../Types';
-
-const SENTRYCLIRC_FILENAME = '.sentryclirc';
-const GITIGNORE_FILENAME = '.gitignore';
-const PROPERTIES_FILENAME = 'sentry.properties';
 
 export interface SentryCliProps {
   'defaults/url': string;
@@ -93,71 +86,5 @@ export class SentryCli {
       dumpedSections.push(section);
     }
     return dumpedSections.join('\n');
-  }
-
-  /**
-   * Creates `.sentryclirc` and `sentry.properties` files with the CLI properties
-   * obtained from the user answers (or from logging into Sentry).
-   * The `.sentryclirc` only contains the auth token and will be added to the
-   * user's `.gitignore` file. The properties file contains the rest of the
-   * properties (org, project, etc.).
-   *
-   * @param sentryCli instance of the Sentry CLI
-   * @param cliProps the properties to write to the files
-   */
-  public async createSentryCliConfig(cliProps: SentryCliProps): Promise<void> {
-    const { 'auth/token': authToken, ...cliPropsToWrite } = cliProps;
-
-    /**
-     * To not commit the auth token to the VCS, instead of adding it to the
-     * properties file (like the rest of props), it's added to the Sentry CLI
-     * config, which is added to the gitignore. This way makes the properties
-     * file safe to commit without exposing any auth tokens.
-     */
-    if (authToken) {
-      try {
-        await fs.promises.appendFile(
-          SENTRYCLIRC_FILENAME,
-          this.dumpConfig({ auth: { 'auth/token': authToken } }),
-        );
-        green(`✓ Successfully added the auth token to ${SENTRYCLIRC_FILENAME}`);
-      } catch {
-        red(
-          `⚠ Could not add the auth token to ${SENTRYCLIRC_FILENAME}, ` +
-            `please add it to identify your user account:\n${authToken}`,
-        );
-        nl();
-      }
-    } else {
-      red(
-        `⚠ Did not find an auth token, please add your token to ${SENTRYCLIRC_FILENAME}`,
-      );
-      l(
-        'To generate an auth token, visit https://sentry.io/settings/account/api/auth-tokens/',
-      );
-      l(
-        'To learn how to configure Sentry CLI, visit ' +
-          'https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#configure-sentry-cli',
-      );
-    }
-
-    await addToGitignore(
-      SENTRYCLIRC_FILENAME,
-      `⚠ Could not add ${SENTRYCLIRC_FILENAME} to ${GITIGNORE_FILENAME}, please add it to not commit your auth key.`,
-    );
-
-    try {
-      await fs.promises.writeFile(
-        `./${PROPERTIES_FILENAME}`,
-        this.dumpProperties(cliPropsToWrite),
-      );
-      green('✓ Successfully created sentry.properties');
-    } catch {
-      red(`⚠ Could not add org and project data to ${PROPERTIES_FILENAME}`);
-      l(
-        'See docs for a manual setup: https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#configure-sentry-cli',
-      );
-    }
-    nl();
   }
 }
