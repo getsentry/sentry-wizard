080b5e3685e39aefe60d864a645b839c9d353e43
Onur Temizkan <onur@narval.co.uk>
2025-06-26 16:19:05 +0100

    remove(remix): Drop Remix v1 support

--- src/remix/codemods/root.ts
+++ src/remix/codemods/root.ts
@@ -2,18 +2,78 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
-
 import * as recast from 'recast';
 import * as path from 'path';
 
 import type { ExportNamedDeclaration, Program } from '@babel/types';
 
-// @ts-expect-error - magicast is ESM and TS complains about that. It works though
-import { loadFile, writeFile } from 'magicast';
+import {
+  builders,
+  generateCode,
+  loadFile,
+  ProxifiedModule,
+  writeFile,
+  // @ts-expect-error - magicast is ESM and TS complains about that. It works though
+} from 'magicast';
 
 import { ERROR_BOUNDARY_TEMPLATE_V2 } from '../templates';
 import { hasSentryContent } from '../utils';
-import { wrapAppWithSentry } from './root-common';
+import chalk from 'chalk';
+
+// @ts-expect-error - clack is ESM and TS complains about that. It works though
+import * as clack from '@clack/prompts';
+
+export function wrapAppWithSentry(
+  rootRouteAst: ProxifiedModule,
+  rootFileName: string,
+) {
+  rootRouteAst.imports.$add({
+    from: '@sentry/remix',
+    imported: 'withSentry',
+    local: 'withSentry',
+  });
+
+  recast.visit(rootRouteAst.$ast, {
+    visitExportDefaultDeclaration(path) {
+      if (path.value.declaration.type === 'FunctionDeclaration') {
+        // Move the function declaration just before the default export
+        path.insertBefore(path.value.declaration);
+
+        // Get the name of the function to be wrapped
+        const functionName: string = path.value.declaration.id.name as string;
+
+        // Create the wrapped function call
+        const functionCall = recast.types.builders.callExpression(
+          recast.types.builders.identifier('withSentry'),
+          [recast.types.builders.identifier(functionName)],
+        );
+
+        // Replace the default export with the wrapped function call
+        path.value.declaration = functionCall;
+      } else if (path.value.declaration.type === 'Identifier') {
+        const rootRouteExport = rootRouteAst.exports.default;
+
+        const expressionToWrap = generateCode(rootRouteExport.$ast).code;
+
+        rootRouteAst.exports.default = builders.raw(
+          `withSentry(${expressionToWrap})`,
+        );
+      } else {
+        clack.log.warn(
+          chalk.yellow(
+            `Couldn't instrument ${chalk.bold(
+              rootFileName,
+            )} automatically. Wrap your default export with: ${chalk.dim(
+              'withSentry()',
+            )}\n`,
+          ),
+        );
+      }
+
+      this.traverse(path);
+    },
+  });
+}
 
 export async function instrumentRootRouteV2(
   rootFileName: string,
--- src/remix/sdk-setup.ts
+++ src/remix/sdk-setup.ts
@@ -30,8 +30,7 @@ import {
   hasSentryContent,
   serverHasInstrumentationImport,
 } from './utils';
-import { instrumentRootRouteV1 } from './codemods/root-v1';
-import { instrumentRootRouteV2 } from './codemods/root-v2';
+import { instrumentRootRouteV2 } from './codemods/root';
 import { instrumentHandleError } from './codemods/handle-error';
 import { getPackageDotJson } from '../utils/clack';
 import { findCustomExpressServerImplementation } from './codemods/express-server';
@@ -256,10 +255,7 @@ Skipping adding instrumentation functionality to ${chalk.cyan(
   return true;
 }
 
-export function isRemixV2(
-  remixConfig: PartialRemixConfig,
-  packageJson: PackageDotJson,
-): boolean {
+export function isRemixV2(packageJson: PackageDotJson): boolean {
   const remixVersion = getPackageVersion('@remix-run/react', packageJson);
   if (!remixVersion) {
     return false;
@@ -271,9 +267,7 @@ export function isRemixV2(
     return false;
   }
 
-  const isV2Remix = gte(minVer, '2.0.0');
-
-  return isV2Remix || remixConfig?.future?.v2_errorBoundary || false;
+  return gte(minVer, '2.0.0');
 }
 
 export async function loadRemixConfig(): Promise<PartialRemixConfig> {
@@ -297,8 +291,8 @@ export async function loadRemixConfig(): Promise<PartialRemixConfig> {
         typeof e === 'object' && e != null && 'toString' in e
           ? e.toString()
           : typeof e === 'string'
-          ? e
-          : 'Unknown error',
+            ? e
+            : 'Unknown error',
       ),
     );
 
@@ -306,17 +300,10 @@ export async function loadRemixConfig(): Promise<PartialRemixConfig> {
   }
 }
 
-export async function instrumentRootRoute(
-  isV2?: boolean,
-  isTS?: boolean,
-): Promise<void> {
+export async function instrumentRootRoute(isTS?: boolean): Promise<void> {
   const rootFilename = `root.${isTS ? 'tsx' : 'jsx'}`;
 
-  if (isV2) {
-    await instrumentRootRouteV2(rootFilename);
-  } else {
-    await instrumentRootRouteV1(rootFilename);
-  }
+  await instrumentRootRouteV2(rootFilename);
 
   clack.log.success(
     `Successfully instrumented root route ${chalk.cyan(rootFilename)}.`,
@@ -512,7 +499,6 @@ export async function updateStartScript(instrumentationFile: string) {
 }
 
 export async function instrumentSentryOnEntryServer(
-  isV2: boolean,
   isTS: boolean,
 ): Promise<void> {
   const serverEntryFilename = `entry.server.${isTS ? 'tsx' : 'jsx'}`;
@@ -535,19 +521,17 @@ export async function instrumentSentryOnEntryServer(
     local: 'Sentry',
   });
 
-  if (isV2) {
-    const handleErrorInstrumented = instrumentHandleError(
-      originalEntryServerMod,
-      serverEntryFilename,
-    );
+  const handleErrorInstrumented = instrumentHandleError(
+    originalEntryServerMod,
+    serverEntryFilename,
+  );
 
-    if (handleErrorInstrumented) {
-      clack.log.success(
-        `Instrumented ${chalk.cyan('handleError')} in ${chalk.cyan(
-          `${serverEntryFilename}`,
-        )}`,
-      );
-    }
+  if (handleErrorInstrumented) {
+    clack.log.success(
+      `Instrumented ${chalk.cyan('handleError')} in ${chalk.cyan(
+        `${serverEntryFilename}`,
+      )}`,
+    );
   }
 
   await writeFile(
