05c484d2d8854dcce2bf630c4be8523e16969220
Onur Temizkan <onur@narval.co.uk>
2025-08-05 11:38:54 +0100

    feat(nextjs): Set `sendDefaultPii: true` by default

--- src/nextjs/templates.ts
+++ src/nextjs/templates.ts
@@ -174,6 +174,10 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
 });
 `;
 }
@@ -230,6 +234,10 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;`;
