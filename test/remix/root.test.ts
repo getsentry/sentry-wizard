7bc335846bf6f0e2e9c5a2466139e737c439c012
Onur Temizkan <onur@narval.co.uk>
2025-08-07 09:39:25 +0100

    Add tests

--- test/remix/root.test.ts
+++ test/remix/root.test.ts
@@ -1,7 +1,10 @@
 // @ts-expect-error - magicast is ESM and TS complains about that. It works though
 import { parseModule } from 'magicast';
 import { describe, expect, it } from 'vitest';
-import { wrapAppWithSentry } from '../../src/remix/codemods/root';
+import {
+  wrapAppWithSentry,
+  isWithSentryAlreadyUsed,
+} from '../../src/remix/codemods/root';
 
 describe('wrapAppWithSentry', () => {
   it('should wrap the app with Sentry', () => {
@@ -28,5 +31,99 @@ describe('wrapAppWithSentry', () => {
 
       export default withSentry(App);"
     `);
+  });
+});
+
+describe('isWithSentryAlreadyUsed', () => {
+  it('should return false when withSentry is not used', () => {
+    const rootAst = parseModule(`
+      import { Outlet } from '@remix-run/react';
+
+      export default function App() {
+        return <Outlet />;
+      }
+    `);
+
+    expect(isWithSentryAlreadyUsed(rootAst)).toBe(false);
+  });
+
+  it('should return true when withSentry is used in default export', () => {
+    const rootAst = parseModule(`
+      import { withSentry } from '@sentry/remix';
+      import { Outlet } from '@remix-run/react';
+
+      function App() {
+        return <Outlet />;
+      }
+
+      export default withSentry(App);
+    `);
+
+    expect(isWithSentryAlreadyUsed(rootAst)).toBe(true);
+  });
+
+  it('should return true when withSentry is used in a variable assignment', () => {
+    const rootAst = parseModule(`
+      import { withSentry } from '@sentry/remix';
+      import { Outlet } from '@remix-run/react';
+
+      function App() {
+        return <Outlet />;
+      }
+
+      const WrappedApp = withSentry(App);
+      export default WrappedApp;
+    `);
+
+    expect(isWithSentryAlreadyUsed(rootAst)).toBe(true);
+  });
+
+  it('should return true when withSentry is used inside a function', () => {
+    const rootAst = parseModule(`
+      import { withSentry } from '@sentry/remix';
+      import { Outlet } from '@remix-run/react';
+
+      function App() {
+        return <Outlet />;
+      }
+
+      function createApp() {
+        return withSentry(App);
+      }
+
+      export default createApp();
+    `);
+
+    expect(isWithSentryAlreadyUsed(rootAst)).toBe(true);
+  });
+
+  it('should return false when withSentry is imported but not used', () => {
+    const rootAst = parseModule(`
+      import { withSentry } from '@sentry/remix';
+      import { Outlet } from '@remix-run/react';
+
+      export default function App() {
+        return <Outlet />;
+      }
+    `);
+
+    expect(isWithSentryAlreadyUsed(rootAst)).toBe(false);
+  });
+
+  it('should return false when a different function with similar name is used', () => {
+    const rootAst = parseModule(`
+      import { Outlet } from '@remix-run/react';
+
+      function withSentryLike() {
+        return null;
+      }
+
+      export default function App() {
+        withSentryLike();
+        return <Outlet />;
+      }
+    `);
+
+    expect(isWithSentryAlreadyUsed(rootAst)).toBe(false);
   });
 });
