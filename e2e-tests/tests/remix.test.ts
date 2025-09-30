4bf6454632cba85c8879158d7212b2036a771419
Onur Temizkan <onur@narval.co.uk>
2025-09-30 12:30:41 +0100

    Revert remix test updates

--- e2e-tests/tests/remix.test.ts
+++ e2e-tests/tests/remix.test.ts
@@ -179,17 +179,17 @@ function checkRemixProject(
 
   test('entry.client file contains Sentry initialization', () => {
     checkFileContents(`${projectDir}/app/entry.client.tsx`, [
-      'import * as Sentry from "@sentry/remix";',
-      `Sentry.init({
+      'import { init, replayIntegration, browserTracingIntegration } from "@sentry/remix";',
+      `init({
     dsn: "${TEST_ARGS.PROJECT_DSN}",
     tracesSampleRate: 1,
     enableLogs: true,
 
-    integrations: [Sentry.browserTracingIntegration({
+    integrations: [browserTracingIntegration({
       useEffect,
       useLocation,
       useMatches
-    }), Sentry.replayIntegration({
+    }), replayIntegration({
         maskAllText: true,
         blockAllMedia: true
     })],
